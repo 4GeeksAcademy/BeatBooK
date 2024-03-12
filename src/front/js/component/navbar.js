35116267774f949d582a17abd6c1506205debfa2
Heyson Betancourt <146369401+heysonbr@users.noreply.github.com>
2024-03-12 17:46:56 +0000

    Añadir Bootstrap, React Bootstrap y React Icons a las dependencias y actualizar la barra de navegación

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Navbar, Nav, FormControl, Button, Form } from "react-bootstrap";
 
-export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+export const AppNavbar = () => {
+  return (
+    <Navbar
+      bg="light"
+      expand="lg"
+      className="rounded"
+      style={{ justifyContent: "space-between" }}
+    >
+      {/* Logo o nombre de la aplicación */}
+      <Navbar.Brand href="/">Mi App</Navbar.Brand>
+
+      {/* Botón para colapsar el menú en dispositivos pequeños */}
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+      {/* Contenido del menú */}
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          {/* Vistas de la página */}
+          <Nav.Link href="/">Inicio</Nav.Link>
+          <Nav.Link href="/recomendados">Recomendados</Nav.Link>
+        </Nav>
+
+        {/* Barra de búsqueda */}
+        <Form className="mb-3 d-flex" style={{ width: "50%" }}>
+          <FormControl
+            type="text"
+            placeholder="Buscar"
+            aria-label="Buscar"
+            aria-describedby="basic-addon2"
+            className="mr-sm-2"
+          />
+          <Button variant="outline-success">
+            <span className="glyphicon glyphicon-search"></span>
+          </Button>
+        </Form>
+
+        {/* Botones de registro e inicio de sesión */}
+        <Nav>
+          <Nav.Link href="/registro">Registro</Nav.Link>
+          <Nav.Link href="/inicio-sesion">Iniciar Sesión</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
