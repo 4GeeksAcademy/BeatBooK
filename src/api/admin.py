a813e752a115607b2a2044084a7d9771349e6bf5
Luis Guilarte Rodriguez <147260322+Luisgr10@users.noreply.github.com>
2024-03-28 11:06:56 +0000

    He creado las rutas de las bandas solo faltan dos

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Band, Event
+from .models import db, User, Band, Event, MusicalCategory
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -14,6 +14,7 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Band, db.session))
     admin.add_view(ModelView(Event, db.session))
+    admin.add_view(ModelView(MusicalCategory, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -1,15 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import LargeBinary
+import json
+
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.LargeBinary)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    profile_image_url = db.Column(db.String(500), unique=False, nullable=True)
+    password = db.Column(db.String(120), nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False)
+    profile_image_url = db.Column(db.String(500), nullable=True)
     band_id = db.Column(db.Integer, db.ForeignKey('band.id'), nullable=True)
+    user_categories = db.relationship('MusicalCategory', secondary='user_favorite_category', back_populates='users')
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -19,25 +22,20 @@ class User(db.Model):
             "id": self.id,
             "username": self.username,
             "email": self.email,
-            "profile_image_url": self.profile_image_url,  # include in serialized output
-            # do not serialize the password, its a security breach
+            "profile_image_url": self.profile_image_url,
         }
-     
-# Definición de la tabla auxiliar para la relación de muchos a muchos de band a event
-band_events = db.Table('band_events',
-    db.Column('band_id', db.Integer, db.ForeignKey('band.id'), primary_key=True),
-    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True)
-)
+
 
 class Band(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(120), unique=True, nullable=False)
-    profile_picture = db.Column(db.String(120), nullable=False)
-    banner_picture = db.Column(db.String(120), nullable=False)
-    social_networks = db.Column(db.String(120), nullable=False)
+    profile_picture = db.Column(db.String(1200), nullable=False)
+    banner_picture = db.Column(db.String(1200), nullable=False)
+    social_networks = db.Column(db.String(1200), nullable=False)
     users = db.relationship('User', backref='band', lazy=False)
-    events = db.relationship('Event', secondary=band_events, back_populates='bands')
+    events = db.relationship('Event', secondary='band_events', back_populates='bands')
+    musical_categories = db.relationship('MusicalCategory', secondary='band_musical_category', back_populates='bands')
 
     def __repr__(self):
         return f'<Band {self.name}>'
@@ -46,20 +44,22 @@ class Band(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "email": self.email
+            "email": self.email,
+            "profile_picture": self.profile_picture,
         }
+
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
     place = db.Column(db.String(120), nullable=False)
-    pictures = db.Column(db.String(120), unique=True, nullable=False)
+    pictures = db.Column(db.String(120), nullable=False)
     media = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String(120), nullable=False)
     date = db.Column(db.Date, nullable=False)
     social_networks = db.Column(db.String(120), nullable=False)
-    price = db.Column(db.String(120), unique=True, nullable=False)
-    bands = db.relationship('Band', secondary=band_events, back_populates='events')
+    price = db.Column(db.String(120), nullable=False)
+    bands = db.relationship('Band', secondary='band_events', back_populates='events')
 
     def __repr__(self):
         return f'<Event {self.name}>'
@@ -70,6 +70,32 @@ class Event(db.Model):
             "name": self.name,
             "place": self.place
         }
-    
 
+
+class MusicalCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    bands = db.relationship('Band', secondary='band_musical_category', back_populates='musical_categories')
+    users = db.relationship('User', secondary='user_favorite_category', back_populates='user_categories')
+
+# Tablas auxiliares 
+band_events = db.Table('band_events',
+    db.Column('band_id', db.Integer, db.ForeignKey('band.id'), primary_key=True),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True)
+)
+
+band_musical_category = db.Table('band_musical_category',
+    db.Column('band_id', db.Integer, db.ForeignKey('band.id'), primary_key=True),
+    db.Column('musical_category_id', db.Integer, db.ForeignKey('musical_category.id'), primary_key=True)
+)
+
+user_favorite_category = db.Table('user_favorite_category',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('musical_category_id', db.Integer, db.ForeignKey('musical_category.id'), primary_key=True)
+)
+
+
+
+
+    
 
