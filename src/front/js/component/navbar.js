699fd68451603f4470286ac20d469dcf193a5ee0
Heyson Betancourt <146369401+heysonbr@users.noreply.github.com>
2024-03-29 18:40:33 +0000

    Actualiza la barra de navegación y los estilos de búsqueda

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,44 +5,49 @@ import { Search } from "./navbar/search";
 import { Login } from "./navbar/login";
 import "./navbar/buttonJoin.css";
 import LogoHorizontal from "./navbar/beatBoxHorizontal.png";
+import LogoHorizontalBlanco from "./navbar/beatBoxHorizontalBlanco.png";
 import "./navbar/logo.css";
 import { SecondaryNavbar } from "./navbar/navbarSecondary";
 
 export const AppNavbar = () => {
   return (
-    <div className="container mb-3 mt-3">
-      <div className="row  d-flex justify-content-between">
+    <div>
+      <div className="row d-flex justify-content-between">
         <nav
-          className="navbar navbar-expand bg-light rounded-top p-3 d-flex justify-content-between mt-3"
+          className="navbar navbar-expand bg-black rounded-top p-3 d-flex justify-content-between align-items-center"
           style={{
             boxShadow: "0 0 10px rgba(0,0,0,0.1)",
             marginBottom: "0",
             paddingBottom: "0",
           }}
         >
-          {/* Logo o nombre de la aplicación */}
-          <Link to="/" className="navbar-brand">
-            <img
-              src={LogoHorizontal}
-              alt="My Image"
-              className="logoHorizontal"
-            />
-          </Link>
-
+          {" "}
+          <div>
+            {/* Logo o nombre de la aplicación */}
+            <Link to="/" className="ps-3">
+              <img
+                src={LogoHorizontalBlanco}
+                alt="My Image"
+                className="logoHorizontal"
+              />
+            </Link>
+            {/* <button className="white-button text-center text-nowrap ">
+              Todas las categorías
+            </button> */}
+          </div>
           {/* Barra de búsqueda */}
-          <div className="d-flex flex-grow-1 justify-content-center">
+          <div className="">
             <Search />
           </div>
-
           {/* Botones de registro e inicio de sesión */}
-          <div className="d-flex justify-content-end">
+          <div className="">
             <Login />
           </div>
         </nav>
       </div>
-      <div className="row">
-        <SecondaryNavbar />
-      </div>
+      {/* <div className="row">
+       <SecondaryNavbar />
+       </div> */}
     </div>
   );
 };
