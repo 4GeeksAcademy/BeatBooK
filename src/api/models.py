bae4d1859efde17add8f9b6f537c5816ab85a189
Miriam Asencio <148205684+Miritzila@users.noreply.github.com>
2024-03-29 19:09:37 +0000

    modelos

--- src/api/models.py
+++ src/api/models.py
@@ -16,47 +16,77 @@ class User(db.Model):
     banner_picture = db.Column(db.String(120), nullable=True)
     social_networks = db.Column(db.String(120), nullable=True)
 
-    events = db.relationship('Event', backref='user_events', lazy=True)
-    places = db.relationship('Place', backref='user_places', lazy=True)
-    
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
     date = db.Column(db.Date, nullable=False)
     description = db.Column(db.String(120), nullable=False)
-    place = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
     price = db.Column(db.String(120), nullable=False)
-    pictures = db.Column(db.String(120), unique=True, nullable=True)
+    pictures = db.Column(db.String(120), nullable=True)
     media = db.Column(db.String(120), nullable=True)
     social_networks = db.Column(db.String(120), nullable=True)
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
     place_id = db.Column(db.Integer, db.ForeignKey('place.id'), nullable=True)
     band_id = db.Column(db.Integer, db.ForeignKey('band.id'), nullable=True)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'date': self.date,
+            'description': self.description,
+            'address': self.address,
+            'price': self.price,
+            'pictures': self.pictures,
+            'media': self.media,
+            'social_networks': self.social_networks,
+            'place_id': self.place_id,
+        }
+
 class Place(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)  # Corregido de "adress" a "address"
+    address = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120), unique=True, nullable=True)
     profile_picture = db.Column(db.String(120), nullable=True)
     banner_picture = db.Column(db.String(120), nullable=True)
     social_networks = db.Column(db.String(120), nullable=True)
 
-    events = db.relationship('Event', backref='event_place', lazy=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    events = db.relationship('Event', backref='place', lazy=True)
+    
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'address': self.address,
+            'phone': self.phone,
+            'profile_picture': self.profile_picture,
+            'banner_picture': self.banner_picture,
+            'social_networks': self.social_networks,
+        }
 
 class Band(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String(120), nullable=False)
     profile_picture = db.Column(db.String(120), nullable=True)
     banner_picture = db.Column(db.String(120), nullable=True)
     social_networks = db.Column(db.String(120), nullable=True)
 
-    events = db.relationship('Event', backref='event_band', lazy=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    events = db.relationship('Event', backref='band', lazy=True)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'profile_picture': self.profile_picture,
+            'banner_picture': self.banner_picture,
+            'social_networks': self.social_networks,
+        }
 
 class Assistance(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -66,6 +96,13 @@ class Assistance(db.Model):
     user = db.relationship('User', backref='assistances', lazy=True)
     event = db.relationship('Event', backref='assistances', lazy=True)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'event_id': self.event_id,
+        }
+
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer, nullable=False)
@@ -73,3 +110,15 @@ class Review(db.Model):
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+
+    user = db.relationship('User', backref='reviews', lazy=True)
+    event = db.relationship('Event', backref='reviews', lazy=True)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'rating': self.rating,
+            'comment': self.comment,
+            'user_id': self.user_id,
+            'event_id': self.event_id,
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Event, Place, Band, Review
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -11,12 +11,48 @@ api = Blueprint('api', __name__)
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route('/events', methods=['GET'])
+def get_events():
+    events = Event.query.all()
+    events = list(map(lambda x: x.serialize(), events))
+    return jsonify(events), 200  
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/events/<int:event_id>', methods=['GET'])
+def get_event(event_id):
+    event = Event.query.get(event_id)
+    if event is None:
+        raise APIException('Event not found', status_code=404)
+    return jsonify(event.serialize()), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route('/events', methods=['POST'])
+def create_event():
+    request_body = request.get_json()
+    event = Event(name=request_body['name'], date=request_body['date'], description=request_body['description'], address=request_body['address'], price=request_body['price'], user_id=request_body['user_id'], place_id=request_body['place_id'])
+    db.session.add(event)
+    db.session.commit()
+    return jsonify(event.serialize()), 201
 
-    return jsonify(response_body), 200
+@api.route('/events/<int:event_id>', methods=['PUT'])
+def update_event(event_id):
+    event = Event.query.get(event_id)
+    if event is None:
+        raise APIException('Event not found', status_code=404)
+    request_body = request.get_json()
+    event.name = request_body['name']
+    event.date = request_body['date']
+    event.description = request_body['description']
+    event.address = request_body['address']
+    event.price = request_body['price']
+    event.place_id = request_body['place_id']
+    db.session.commit()
+    return jsonify(event.serialize()), 200
+
+@api.route('/events/<int:event_id>', methods=['DELETE'])
+def delete_event(event_id):
+    event = Event.query.get(event_id)
+    if event is None:
+        raise APIException('Event not found', status_code=404)
+    db.session.delete(event)
+    db.session.commit()
+    return jsonify(event.serialize()), 200
+
