9df575f06a42b41d0acc16cb52f62a45abe22166
Miriam Asencio <148205684+Miritzila@users.noreply.github.com>
2024-03-31 11:34:45 +0000

    modelos ok v1

--- src/api/models.py
+++ src/api/models.py
@@ -1,20 +1,35 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import LargeBinary
+
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.LargeBinary)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
     birthdate = db.Column(db.Date, nullable=False)
     description = db.Column(db.String(120), nullable=True)
     gender = db.Column(db.String(20), nullable=True)
     city = db.Column(db.String(100), nullable=True)
-    profile_image_url = db.Column(db.String(500), unique=False, nullable=True)  # new field
+    profile_picture = db.Column(db.String(120), nullable=True)
     banner_picture = db.Column(db.String(120), nullable=True)
     social_networks = db.Column(db.String(120), nullable=True)
+
+    user_categories = db.relationship('MusicalCategory', secondary='user_favorite_category', back_populates='users')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'is_active': self.is_active,
+            'email': self.email,
+            'username': self.username,
+            'birthdate': self.birthdate,
+            'description': self.description,
+        }
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,22 +44,6 @@ class Event(db.Model):
 
     place_id = db.Column(db.Integer, db.ForeignKey('place.id'), nullable=True)
     band_id = db.Column(db.Integer, db.ForeignKey('band.id'), nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'date': self.date,
-            'description': self.description,
-            'address': self.address,
-            'price': self.price,
-            'pictures': self.pictures,
-            'media': self.media,
-            'social_networks': self.social_networks,
-            'user_id': self.user_id,
-            'place_id': self.place_id,
-        }
 
     def serialize(self):
         return {
@@ -93,6 +92,10 @@ class Band(db.Model):
     social_networks = db.Column(db.String(120), nullable=True)
 
     events = db.relationship('Event', backref='band', lazy=True)
+    musical_categories = db.relationship('MusicalCategory', secondary='band_musical_category', back_populates='bands')
+
+    members = db.relationship('User', secondary='band_members', backref=db.backref('bands', lazy='dynamic'))
+
 
     def serialize(self):
         return {
@@ -106,8 +109,8 @@ class Band(db.Model):
 
 class Assistance(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=True)
 
     user = db.relationship('User', backref='assistances', lazy=True)
     event = db.relationship('Event', backref='assistances', lazy=True)
@@ -124,8 +127,8 @@ class Review(db.Model):
     rating = db.Column(db.Integer, nullable=False)
     comment = db.Column(db.String(120), nullable=False)
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=True)
 
     user = db.relationship('User', backref='reviews', lazy=True)
     event = db.relationship('Event', backref='reviews', lazy=True)
@@ -138,3 +141,35 @@ class Review(db.Model):
             'user_id': self.user_id,
             'event_id': self.event_id,
         }
+
+class MusicalCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    bands = db.relationship('Band', secondary='band_musical_category', back_populates='musical_categories')
+    users = db.relationship('User', secondary='user_favorite_category', back_populates='user_categories')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+band_events = db.Table('band_events',
+    db.Column('band_id', db.Integer, db.ForeignKey('band.id'), primary_key=True),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True)
+)
+
+band_musical_category = db.Table('band_musical_category',
+    db.Column('band_id', db.Integer, db.ForeignKey('band.id'), primary_key=True),
+    db.Column('musical_category_id', db.Integer, db.ForeignKey('musical_category.id'), primary_key=True)
+)
+
+user_favorite_category = db.Table('user_favorite_category',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('musical_category_id', db.Integer, db.ForeignKey('musical_category.id'), primary_key=True)
+)
+
+band_members = db.Table('band_members',
+    db.Column('band_id', db.Integer, db.ForeignKey('band.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Event, Place, Band, Review
+from api.models import db, User, Event, Place, Band, Review, Assistance, MusicalCategory
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token
@@ -13,41 +13,254 @@ import bcrypt
 import cloudinary
 from cloudinary.uploader import upload
 import os
-          
-cloudinary.config( 
-  cloud_name = "daxbjkj1j", 
- api_key = os.environ["CLOUDINARY_API_KEY"], 
-  api_secret = os.environ["CLOUDINARY_API_SECRET"]  
-)
 
+  
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+#LOGIN Y PRIVADOS#
+
+@api.route('/log_in', methods=['POST'])
+def log_in():
+    request_body = request.get_json()
+    if not 'email' in request_body:
+        return jsonify("Email is required"), 400
+    if not 'password' in request_body:
+        return jsonify("Password is required"), 400
+    user = User.query.filter_by(email=request_body["email"]).first()
+    if user is None or not bcrypt.checkpw(request_body["password"].encode('utf-8'), user.password):
+        return jsonify("Invalid email or password"), 400
+    # Genera un token para el usuario que inició sesión
+    access_token = create_access_token(identity=str(user.id))
+    return jsonify({ 'message': 'Logged in successfully', 'token': access_token, 'email': user.email }), 200
+
+@api.route("/private", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    if user is None:
+        return jsonify({"error": "User not found"}), 404
+    return jsonify({
+        "id": user.id, 
+        "email": user.email,
+        "profile_picture": user.profile_picture,
+        "social_networks": user.social_networks,
+        "users": [user.serialize() for user in user.users],
+    }), 200
+
+@api.route('/upload_profile_image', methods=['POST'])
+@jwt_required()
+def upload_profile_image():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image provided"}), 400
+    file = request.files['image']
+    upload_result = upload(file)
+    url = upload_result['url']
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    if user is None:
+        return jsonify({"error": "User not found"}), 404
+    user.profile_image_url = url
+    db.session.commit()
+    return jsonify({"message": "Profile image uploaded successfully", "url": url}), 200
+
+#USER#
+
+@api.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    users = list(map(lambda x: x.serialize(), users))
+    return jsonify(users), 200
+
+@api.route('/users/<int:user_id>', methods=['GET'])
+def get_single_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException('User not found', status_code=404)
+    return jsonify(user.serialize()), 200
+
+@api.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException('User not found', status_code=404)
+    request_body = request.get_json()
+    user.email = request_body['email']
+    user.username = request_body['username']
+    user.birthdate = request_body['birthdate']
+    user.description = request_body['description']
+    user.gender = request_body['gender']
+    user.city = request_body['city']
+    user.profile_picture = request_body['profile_picture']
+    user.banner_picture = request_body['banner_picture']
+    user.social_networks = request_body['social_networks']
+    db.session.commit()
+    return jsonify(user.serialize()), 200
+
+@api.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify(user.serialize()), 200
+
+@api.route('/users/<int:user_id>/assistance', methods=['GET'])
+def get_user_assistance(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException('User not found', status_code=404)
+    assistances = user.assistances
+    events = []
+    for assistance in assistances:
+        event = assistance.event
+        if event:
+            events.append(event.serialize())
+    return jsonify(events), 200
+
+@api.route('/users/<int:user_id>/favorite_categories', methods=['GET'])
+def get_user_favorite_categories(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException('User not found', status_code=404)
+    categories = user.user_categories
+    categories = list(map(lambda x: x.serialize(), categories))
+    return jsonify(categories), 200
+
+#BANDS#
+
+@api.route('/bands', methods=['GET']) 
+def get_all_bands():
+    bands = Band.query.all()
+    if not bands:
+            return jsonify({"message": "No se encontraron bandas"}), 404    
+    bands_json = [band.serialize() for band in bands]
+    return jsonify(bands_json)
+
+@api.route('/bands/<int:band_id>', methods=['GET'])
+def get_single_band(band_id):
+    band = Band.query.get(band_id)
+    if not band:
+        return jsonify({"message": "Banda no encontrada"}), 404
+    return jsonify(band.serialize()), 200
+
+@api.route('/bands', methods=['POST'])
+def create_band():
+    data = request.json
+    band = Band(
+        name=data.get('name'),
+        description=data.get('description'),
+        profile_picture=data.get('profile_picture'),
+        banner_picture=data.get('banner_picture'),
+        social_networks=data.get('social_networks')
+    )
+    db.session.add(band)
+    db.session.commit()
+    return jsonify(band.serialize()), 201
+
+@api.route('/bands/<int:band_id>', methods=['PUT'])
+def update_band(band_id):
+    band = Band.query.get(band_id)
+    if not band:
+        return jsonify({"message": "Banda no encontrada"}), 404
+    data = request.json
+    band.name = data.get('name')
+    band.description = data.get('description')
+    band.profile_picture = data.get('profile_picture')
+    band.banner_picture = data.get('banner_picture')
+    band.social_networks = data.get('social_networks')
+    db.session.commit()
+    return jsonify(band.serialize()), 200
+
+@api.route('/bands/<int:band_id>', methods=['DELETE'])
+def delete_band(band_id):
+    band = Band.query.get(band_id)
+    if not band:
+        return jsonify({"message": "Banda no encontrada"}), 404
+    db.session.delete(band)
+    db.session.commit()
+    return jsonify(band.serialize()), 200
+
+@api.route('/bands/<int:band_id>/members', methods=['GET'])
+def get_band_members(band_id):
+    band = Band.query.get(band_id)
+    if not band:
+        return jsonify({"message": "Banda no encontrada"}), 404
+    members = band.members
+    members_data = [member.serialize() for member in members]
+    return jsonify(members_data), 200
+
+@api.route('/bands/<int:band_id>/add_member', methods=['POST'])
+def add_band_member(band_id):
+        band = Band.query.get(band_id)
+        if not band:
+            return jsonify({"message": "Banda no encontrada"}), 404
+        data = request.json
+        user_id = data.get('user_id')
+        user = User.query.get(user_id)
+        if not user:
+            return jsonify({"message": "Usuario no encontrado"}), 404
+        if user.band_id:
+            return jsonify({"message": "El usuario ya es miembro de otra banda"}), 400
+        user.band_id = band_id
+        db.session.commit()
+        return jsonify({"message": "Usuario añadido correctamente a la banda"}), 200
+
+@api.route('/bands/<int:band_id>/member/<int:user_id>', methods=['DELETE'])
+def remove_band_member(band_id, user_id):
+    band = Band.query.get(band_id)
+    if band:
+        member = User.query.filter_by(id=user_id, band_id=band_id).first()
+        if member:
+            db.session.delete(member)
+            db.session.commit()
+            return jsonify({'message': 'Miembro eliminado correctamente'}), 200
+        else:
+            return jsonify({'message': 'Miembro no encontrado en la banda'}), 404
+    else:
+        return jsonify({'message': 'Banda no encontrada'}), 404
+    
+@api.route('/bands/<int:band_id>/events', methods=['GET'])
+def get_band_events(band_id):
+    band = Band.query.get(band_id)
+    if not band:
+        return jsonify({"message": "Banda no encontrada"}), 404
+    events = band.events
+    events_data = [event.serialize() for event in events]
+    return jsonify(events_data), 200
+
+#EVENTOS#
+
 @api.route('/events', methods=['GET'])
-def get_events():
+def get_all_events():
     events = Event.query.all()
     events = list(map(lambda x: x.serialize(), events))
-    return jsonify(events), 200  
+    return jsonify(events), 200
 
 @api.route('/events/<int:event_id>', methods=['GET'])
-def get_event(event_id):
+def get_single_event(event_id):
     event = Event.query.get(event_id)
     if event is None:
         raise APIException('Event not found', status_code=404)
     return jsonify(event.serialize()), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
-
-@api.route('/sign_up', methods=['POST'])
-def sign_up():
+@api.route('/events', methods=['POST'])
+def create_event():
     request_body = request.get_json()
-    event = Event(name=request_body['name'], date=request_body['date'], description=request_body['description'], address=request_body['address'], price=request_body['price'], user_id=request_body['user_id'], place_id=request_body['place_id'])
+    event = Event(
+        name=request_body['name'], 
+        date=request_body['date'], 
+        description=request_body['description'], 
+        address=request_body['address'], 
+        price=request_body['price'], 
+        pictures=request_body['pictures'], 
+        media=request_body['media'], 
+        social_networks=request_body['social_networks']
+        )
     db.session.add(event)
     db.session.commit()
     return jsonify(event.serialize()), 201
@@ -63,7 +276,9 @@ def update_event(event_id):
     event.description = request_body['description']
     event.address = request_body['address']
     event.price = request_body['price']
-    event.place_id = request_body['place_id']
+    event.pictures = request_body['pictures']
+    event.media = request_body['media']
+    event.social_networks = request_body['social_networks']
     db.session.commit()
     return jsonify(event.serialize()), 200
 
@@ -76,68 +291,74 @@ def delete_event(event_id):
     db.session.commit()
     return jsonify(event.serialize()), 200
 
-    return jsonify({"message": "Profile image uploaded successfully", "url": url}), 200
+@api.route('/events/<int:event_id>/assistances', methods=['GET'])
+def get_event_assistances(event_id):
+    event = Event.query.get(event_id)
+    if event is None:
+        raise APIException('Event not found', status_code=404)
+    assistances = event.assistances
+    assistances = list(map(lambda x: x.serialize(), assistances))
+    return jsonify(assistances), 200
 
+@api.route('/events/<int:event_id>/reviews', methods=['GET'])
+def get_event_reviews(event_id):
+    event = Event.query.get(event_id)
+    if event is None:
+        raise APIException('Event not found', status_code=404)
+    reviews = event.reviews
+    reviews = list(map(lambda x: x.serialize(), reviews))
+    return jsonify(reviews), 200
 
-#----------------------------Creacion de banda--------------------------------------------------------------------------------------------------------#
-@api.route('/bands', methods=['POST'])
-def create_band():
-    data = request.json
-    name = data.get('name')
-    email = data.get('email')
-    profile_picture = data.get('profile_picture')
-    banner_picture = data.get('banner_picture')
-    social_networks = data.get('social_networks')
-
-    if not name or not email or not profile_picture or not banner_picture or not social_networks:
-        return jsonify({'message': 'Todos los campos son requeridos'}), 400
-
-    new_band = Band(name=name, email=email, profile_picture=profile_picture, banner_picture=banner_picture, social_networks=social_networks)
-    db.session.add(new_band)
+@api.route('/events/<int:event_id>/add_assistances', methods=['POST'])
+def add_assistance(event_id):
+    request_body = request.get_json()
+    user_id = request_body['user_id']
+    existing_assistance = Assistance.query.filter_by(user_id=user_id).first()
+    if existing_assistance:
+        return jsonify({"message": "User is already attending an event"}), 400
+    assistance = Assistance(user_id=user_id, event_id=event_id)
+    db.session.add(assistance)
     db.session.commit()
+    return jsonify(assistance.serialize()), 201
 
-    return jsonify({'message': 'Banda creada exitosamente', 'band_id': new_band.id}), 201
-#----------------------------Creacion de banda--------------------------------------------------------------------------------------------------------#
+@api.route('/events/<int:event_id>/remove_assistances', methods=['DELETE'])
+def remove_assistance(event_id):
+    request_body = request.get_json()
+    user_id = request_body['user_id']
+    assistance = Assistance.query.filter_by(user_id=user_id, event_id=event_id).first()
+    if assistance is None:
+        return jsonify({"message": "User is not attending this event"}), 404
+    db.session.delete(assistance)
+    db.session.commit()
+    return jsonify(assistance.serialize()), 200
 
-#----------------------------Creacion de Evento--------------------------------------------------------------------------------------------------------#
-# Ruta para crear un evento
-@api.route('/events', methods=['POST'])
-def create_event():
-    data = request.json
-    name = data.get('name')
-    place = data.get('place')
-    pictures = data.get('pictures')
-    media = data.get('media')
-    description = data.get('description')
-    date = data.get('date')
-    social_networks = data.get('social_networks')
-    price = data.get('price')
-    band_name = data.get('band_name')
+#PLACES#
 
-    if not name or not place or not pictures or not media or not description or not date or not social_networks or not price or not band_name:
-        return jsonify({'message': 'Todos los campos son requeridos'}), 400
+@api.route('/places', methods=['GET'])
+def get_all_places():
+    places = Place.query.all()
+    serialized_places = [place.serialize() for place in places]
+    return jsonify(serialized_places), 200
 
-    # Busca la banda por su nombre
-    band = Band.query.filter_by(name=band_name).first()
-    if not band:
-        return jsonify({'message': 'No se encontró ninguna banda con el nombre proporcionado'}), 404
+@api.route('/places/<int:place_id>', methods=['GET'])
+def get_single_place(place_id):
+    place = Place.query.get(place_id)
+    if place is None:
+        raise APIException('Place not found', status_code=404)
+    return jsonify(place.serialize()), 200
 
-    new_event = Event(
-        name=name,
-        place=place,
-        pictures=pictures,
-        media=media,
-        description=description,
-        date=date,
-        social_networks=social_networks,
-        price=price
+@api.route('/places', methods=['POST'])
+def create_place():
+    request_body = request.get_json()
+    place = Place(
+        name=request_body['name'],
+        description=request_body['description'],
+        address=request_body['address'],
+        phone=request_body['phone'],
+        profile_picture=request_body['profile_picture'],
+        banner_picture=request_body['banner_picture'],
+        social_networks=request_body['social_networks']
     )
-
-    # Asigna la banda al evento
-    new_event.bands.append(band)
-
-    db.session.add(new_event)
+    db.session.add(place)
     db.session.commit()
-
-    return jsonify({'message': 'Evento creado exitosamente', 'event_id': new_event.id}), 201
-#----------------------------Creacion de Evento--------------------------------------------------------------------------------------------------------#
+    return jsonify(place.serialize()), 201
