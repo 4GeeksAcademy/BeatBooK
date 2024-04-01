64ed25ae8a9c8f4736e38bca029468d05d0d8893
Heyson Betancourt <146369401+heysonbr@users.noreply.github.com>
2024-04-01 17:40:01 +0000

    c

--- src/api/models.py
+++ src/api/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.LargeBinary)
+    # password = db.Column(db.String(120), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     birthdate = db.Column(db.Date, nullable=True)
     description = db.Column(db.String(120), nullable=True)
