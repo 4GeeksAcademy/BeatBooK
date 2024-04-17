470e7fd58104c8aa9712c76c973695b731d0ebdb
Heyson Betancourt <146369401+heysonbr@users.noreply.github.com>
2024-04-17 09:59:31 +0000

    Agregar dependencia @material-ui/icons y actualizar versiones en package-lock.json y package.json

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Search } from "./navbar/search";
 import { Login } from "./navbar/login";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./navbar/buttonJoin.css";
 import LogoHorizontal from "./navbar/beatBoxHorizontal.png";
 import LogoHorizontalBlanco from "./navbar/beatBoxHorizontalBlanco.png";
@@ -10,12 +10,23 @@ import "./navbar/logo.css";
 import { SecondaryNavbar } from "./navbar/navbarSecondary";
 import { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Menu, MenuItem, Button, Hidden } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 export const AppNavbar = () => {
   const { store, actions } = useContext(Context);
   useEffect(() => {
     actions.checkUser();
   }, []);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div
       style={{
@@ -45,16 +56,47 @@ export const AppNavbar = () => {
                   className="logoHorizontal"
                 />
               </Link>
-              <Link to="/categorias" className="white-button text-center text-nowrap">
-                Categorías
-              </Link>
-              <Link to="/eventos" className="white-button text-center text-nowrap">
-                Eventos
-              </Link>
-              <Link to="/grupos" className="white-button text-center text-nowrap">
-                Grupos </Link>
-              <Link to="/lugares" className="white-button text-center text-nowrap">
-                Lugares </Link>
+              <Hidden smUp>
+                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} style={{ color: 'white', backgroundColor: 'black' }}>
+
+                  <MenuIcon />
+                </Button>
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/categorias" className="text-black">Categorías</Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose} >
+                    <Link to="/eventos" className="text-black">Eventos</Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/grupos" className="text-black">Grupos</Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <Link to="/lugares" className="text-black">Lugares</Link>
+                  </MenuItem>
+                </Menu>
+              </Hidden>
+
+              <Hidden xsDown>
+                <Link to="/categorias" style={{ textDecoration: 'none' }} className="white-button text-center text-nowrap">
+                  Categorías
+                </Link>
+                <Link to="/eventos" style={{ textDecoration: 'none' }} className="white-button text-center text-nowrap">
+                  Eventos
+                </Link>
+                <Link to="/grupos" style={{ textDecoration: 'none' }} className="white-button text-center text-nowrap">
+                  Grupos
+                </Link>
+                <Link to="/lugares" style={{ textDecoration: 'none' }} className="white-button text-center text-nowrap">
+                  Lugares
+                </Link>
+              </Hidden>
             </div>
             {/* Barra de búsqueda */}
             <div className="d-flex justify-content-center"></div>
